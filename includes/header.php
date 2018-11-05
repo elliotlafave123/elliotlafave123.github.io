b4c8fd3e497183299b89270062010d85519a2755
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2018-11-05 22:27:31 +0000

    .

---
+++ includes/header.php
@@ -0,0 +1,8 @@
+<div id="header_text"><a class="website_logo" href="index.html">Elliotlafave<span class="dotcom">.tk</span></a></div>
+
+<ul> <!------------ contains header navigation menu -------------->
+  <li><a class="active" href="index.html">Home</a></li>
+  <li><a href="news.html">Services</a></li>
+  <li><a href="contact.html">Contact</a></li>
+  <li><a href="about.html">About</a>  </li>
+</ul>
