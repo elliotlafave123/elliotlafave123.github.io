eb47dbb667909c869030216d06e6fe467e4b48df
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2021-12-04 18:18:38 +0000

    Dice Game Mobile Responsive

--- DiceGame/script.js
+++ DiceGame/script.js
@@ -12,6 +12,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
 let scores, currentScore, activePlayer, playing;
 
 // starting conditions
