fe44ea18e3befb36fffe66f5e2c17b06afbeff2b
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2021-12-23 14:55:40 +0000

    Added an all projects program
    A program which shows all my projects and can be filtered through

---
+++ search.js
@@ -0,0 +1,246 @@
+const dataProject0 = {
+    title: "Social Media Dashboard",
+    paragraph:
+        "I created a social media dashboard using HTML, CSS and JS to add data to the webpage.",
+    linkLivePreview: "https://elliotlafave.com/projects/socialmediadashboard/",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/SocialMediaDashboard",
+    linkImg:
+        "https://elliotlafave.com/img/Projects/Social%20Media%20Dashboard.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject1 = {
+    title: "Toast Notification",
+    paragraph:
+        "I creates a toast notification (Popup) using JavaScript event listeners.",
+    linkLivePreview: "Projects/ToastNotification/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/ToastNotification",
+    linkImg: "img/Projects/Toast Notification.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject2 = {
+    title: "Blurry Loading",
+    paragraph:
+        "Blurry loading animation using CSS animation along with JavaScript timing functions.",
+    linkLivePreview: "Projects/BlurryLoading/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/BlurryLoading",
+    linkImg: "img/Projects/Blurry Loading.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject3 = {
+    title: "Rotating Navigation",
+    paragraph:
+        "I created an animated navigation menu that spins out from the side of the page.",
+    linkLivePreview: "Projects/RotatingNavigation/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/RotatingNavigation",
+    linkImg: "img/Projects/Rotating Navigation.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject4 = {
+    title: "Hidden Search Widget",
+    paragraph:
+        "I created a mini hidden search widget that reveals a searchbar when clicked.",
+    linkLivePreview: "Projects/HiddenSearchWidget/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/HiddenSearchWidget",
+    linkImg: "img/Projects/Hidden Search Widget.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject5 = {
+    title: "Scroll Animation",
+    paragraph:
+        "I created a scroll animation that detects when a user is scrolling and shows cards as you scroll.",
+    linkLivePreview: "Projects/ScrollAnimation/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/ScrollAnimation",
+    linkImg: "img/Projects/Scroll Animation.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject6 = {
+    title: "Double Vertical Slider",
+    paragraph:
+        "I created a JavaScript double slider which slides in new content from the top and bottom.",
+    linkLivePreview: "Projects/DoubleVerticalSlider/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/DoubleVerticalSlider",
+    linkImg: "img/Projects/Double Vertical Slider.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject7 = {
+    title: "Expanding Cards",
+    paragraph:
+        "I made different cards that collapse to show only the card which is in focus. ",
+    linkLivePreview: "Projects/ExpandingCards/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/ExpandingCards",
+    linkImg: "img/Projects/Expanding cards.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject8 = {
+    title: "Random Takeaway Picker",
+    paragraph:
+        "I created a random takeaway picker to help me decide what food too order when I'm hungry.",
+    linkLivePreview: "Projects/TakeawayPicker/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/TakeawayPicker",
+    linkImg: "img/Projects/TakeawayPicker.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject9 = {
+    title: "Progress Steps",
+    paragraph:
+        "I created progress steps which can be used in a sign up process or a shopping basket.",
+    linkLivePreview: "Projects/ProgressSteps/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/ProgressSteps",
+    linkImg: "img/Projects/Progress Steps.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject10 = {
+    title: "Checkbox Slider",
+    paragraph:
+        "I designed an animated checkbox slider that can be used anywhere i might need a toggle. I use this for dark mode sliders.",
+    linkLivePreview: "Projects/CheckboxSlider/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/CheckboxSlider",
+    linkImg: "img/Projects/Dark Mode Slider.png",
+    tags: ["html", "css", "js"],
+};
+
+const dataProject11 = {
+    title: "Live User Filter",
+    paragraph:
+        "I used a free API to get random user data such as names and profile pictures and used JS to sort through them.",
+    linkLivePreview: "Projects/LiveUserFilter/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/LiveUserFilter",
+    linkImg: "img/Projects/Live User Filter.png",
+    tags: ["html", "css", "js", "api"],
+};
+
+const dataProject12 = {
+    title: "Movie Search App",
+    paragraph:
+        "I used an API from the movie database to create a movie app which displays the most popular movies and can be searched through by the user.",
+    linkLivePreview: "Projects/MovieApp/index.html",
+    linkGit:
+        "https://github.com/elliotlafave123/elliotlafave123.github.io/tree/master/Projects/MovieApp",
+    linkImg: "img/Projects/Movie App.png",
+    tags: ["html", "css", "js", "api"],
+};
+
+const data = [
+    dataProject0,
+    dataProject1,
+    dataProject2,
+    dataProject3,
+    dataProject4,
+    dataProject5,
+    dataProject6,
+    dataProject7,
+    dataProject8,
+    dataProject9,
+    dataProject10,
+    dataProject11,
+    dataProject12,
+];
+
+const cardsContainer = document.getElementById("cardsContainer");
+const searchInput = document.querySelector(".search-input");
+const dropdown = document.getElementById("dropdown");
+
+const displayMovements = function (data) {
+    cardsContainer.innerHTML = "";
+
+    data.forEach(function (project, i) {
+        console.log();
+        const html = `
+        <div class="card">
+				<img
+					src="${project.linkImg}"
+					alt=""
+				/>
+				<div class="card-content">
+					<h4>${project.title}</h4>
+					<div class="tags-container">
+                        ${
+                            project.tags.includes("html")
+                                ? '<div class="tag tag-html">HTML</div>'
+                                : ""
+                        }
+						${project.tags.includes("css") ? '<div class="tag tag-css">CSS</div>' : ""}
+						${project.tags.includes("js") ? '<div class="tag tag-js">JS</div>' : ""}
+						${project.tags.includes("api") ? '<div class="tag tag-api">API</div>' : ""}
+					</div>
+					<p>
+						${project.paragraph}
+					</p>
+				</div>
+				<div class="buttons-container">
+					<a href="${project.linkLivePreview}" class="btn-live-preview"
+						>Live Preview
+						<i class="fa-solid fa-up-right-from-square"></i>
+					</a>
+					<a href="${project.linkGit}" class="btn-git">
+						<img
+							src="https://d33wubrfki0l68.cloudfront.net/f78349611e6cb9ec98b8b2851b3b1e616e7e4ea3/1c471/img/github-logo.png"
+							alt=""
+						/>
+					</a>
+				</div>
+			</div>
+      `;
+
+        cardsContainer.insertAdjacentHTML("afterbegin", html);
+    });
+};
+displayMovements(data);
+
+searchInput.addEventListener("input", (e) => {
+    e.preventDefault();
+
+    search(searchInput.value);
+});
+
+let isThere = [];
+
+const search = function (input) {
+    isThere = [];
+
+    data.forEach(function (project, i) {
+        let words = project.title.toLowerCase().split(" ");
+
+        if (words.includes(input)) {
+            isThere.push(project);
+        }
+    });
+
+    displayMovements(isThere);
+};
+
+dropdown.addEventListener("input", (e) => {
+    e.preventDefault();
+    let selection = [];
+
+    data.forEach(function (project, i) {
+        if (project.tags.includes(dropdown.value)) {
+            selection.push(project);
+            displayMovements(selection);
+        } else {
+            displayMovements(data);
+        }
+    });
+});
