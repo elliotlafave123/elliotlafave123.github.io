91b0edd673ae7cbe204689449753ebdb7b542043
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2021-12-31 18:06:39 +0000

    .

--- Projects/TakeawayPicker/script.js
+++ Projects/TakeawayPicker/script.js
@@ -2,6 +2,7 @@ const spin = document.getElementById("spin");
 const again = document.getElementById("again");
 const takeawayName = document.querySelector(".name");
 const imgContainer = document.getElementById("imgContainer");
+const main = document.querySelector("main");
 
 const takeaways = ["Dominos", "KFC", "PapaJohns", "SubWay", "McDonald's"];
 
@@ -56,10 +57,23 @@ function getTakeaways() {
 		imgContainer.innerHTML = `<img src="img/" id="img" class="finalImg"/>`;
 		console.log("hello");
 		const img = document.getElementById("img");
+		takeawayName.style.color = "#5f3dc4";
 
 		takeawayName.textContent = takeaways[position];
 		spin.classList.add("hidden");
 		again.classList.remove("hidden");
+
+		if (takeaways[position] === "Dominos") {
+			takeawayName.style.color = "#2C628C";
+		} else if (takeaways[position] === "KFC") {
+			takeawayName.style.color = "#962918";
+		} else if (takeaways[position] === "PapaJohns") {
+			takeawayName.style.color = "#C83834";
+		} else if (takeaways[position] === "SubWay") {
+			takeawayName.style.color = "#265445";
+		} else if (takeaways[position] === "McDonald's") {
+			takeawayName.style.color = "#F7CD3D";
+		}
 
 		img.src = `img/${takeaways[position]}.png`;
 	}, delayInMilliseconds);
