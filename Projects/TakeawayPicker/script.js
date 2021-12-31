851927d5518341c124acbb6b2233ff8f394e8015
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2021-12-31 17:28:39 +0000

    .

--- Projects/TakeawayPicker/script.js
+++ Projects/TakeawayPicker/script.js
@@ -1,19 +1,69 @@
-const spin = document.getElementById('spin')
-const again = document.getElementById('again')
-const takeawayName = document.querySelector('.name')
-const img = document.getElementById('img')
+const spin = document.getElementById("spin");
+const again = document.getElementById("again");
+const takeawayName = document.querySelector(".name");
+const imgContainer = document.getElementById("imgContainer");
 
-const takeaways = ['Dominos', 'KFC', 'PapaJohns', 'SubWay', "McDonald's"]
+const takeaways = ["Dominos", "KFC", "PapaJohns", "SubWay", "McDonald's"];
+
+let delayInMilliseconds = 3000;
 
 function getTakeaways() {
-    position = Math.trunc(Math.random()*takeaways.length)
+	position = Math.trunc(Math.random() * takeaways.length);
+	takeawayName.textContent = `...`;
 
-    takeawayName.textContent = takeaways[position];
-    spin.classList.add('hidden')
-    again.classList.remove('hidden')
+	imgContainer.innerHTML = `
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    <img src="img/PapaJohns.png" id="img" />
+    <img src="img/KFC.png" id="img" />
+    <img src="img/McDonald's.png" id="img" />
+    <img src="img/SubWay.png" id="img" />
+    <img src="img/Dominos.png" id="img" />
+    `;
 
-    img.src=`img/${takeaways[position]}.png`
+	setTimeout(function () {
+		//your code to be executed after 1 second
+		imgContainer.innerHTML = `<img src="img/" id="img" class="finalImg"/>`;
+		console.log("hello");
+		const img = document.getElementById("img");
+
+		takeawayName.textContent = takeaways[position];
+		spin.classList.add("hidden");
+		again.classList.remove("hidden");
+
+		img.src = `img/${takeaways[position]}.png`;
+	}, delayInMilliseconds);
 }
 
-spin.addEventListener('click', getTakeaways)
-again.addEventListener('click', getTakeaways)
+spin.addEventListener("click", getTakeaways);
+again.addEventListener("click", getTakeaways);
