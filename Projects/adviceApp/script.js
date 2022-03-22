be89683c3fd08361e3e8ff16f071377a08f7e934
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-03-22 17:02:16 +0000

    .

---
+++ Projects/adviceApp/script.js
@@ -0,0 +1,24 @@
+const btn = document.querySelector(".btn");
+const adviceText = document.querySelector(".advice");
+const number = document.querySelector("#num");
+
+const showData = function (data) {
+	adviceText.textContent = data.advice;
+	number.textContent = data.id;
+};
+
+const getData = function (x) {
+	fetch(`https://api.adviceslip.com/advice/${x}`)
+		.then((res) => {
+			return res.json();
+		})
+		.then((data) => {
+			console.log(data.slip);
+			showData(data.slip);
+		});
+};
+
+btn.addEventListener("click", function () {
+	let x = Math.random() * 223;
+	getData(x);
+});
