50e23998fc79935516b5b7bef6f0c818f28f7d48
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-03-23 13:54:08 +0000

    .

---
+++ Projects/2048/modal.js
@@ -0,0 +1,32 @@
+const closeModal = document.getElementById("closeModal");
+const modalContainer = document.getElementById("modalContainer");
+const modal = document.querySelector(".modal");
+const openModal = document.getElementById("openModal");
+const reset = document.getElementById("reset");
+
+const closeModalWindow = function () {
+	modalContainer.style.display = "none";
+	modal.classList.add("hidden");
+};
+
+const openModalWindow = function () {
+	modalContainer.style.display = "flex";
+	modal.classList.remove("hidden");
+};
+
+if (!localStorage.getItem("firstGame")) {
+	localStorage.setItem("firstGame", "false");
+}
+
+if (localStorage.getItem("firstGame") == "false") {
+	closeModalWindow();
+}
+
+closeModal.addEventListener("click", closeModalWindow);
+modalContainer.addEventListener("click", closeModalWindow);
+
+openModal.addEventListener("click", openModalWindow);
+
+reset.addEventListener("click", () => {
+	location.reload();
+});
