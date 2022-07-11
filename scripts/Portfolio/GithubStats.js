38a4e78b774a94f9ac55c7c1a50d598680e95778
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-07-11 11:21:46 +0100

    Added a github api

---
+++ scripts/Portfolio/GithubStats.js
@@ -0,0 +1,69 @@
+const GithubStatsContainer = document.getElementById("GithubStats");
+
+function yyyymmdd(dateIn) {
+	var yyyy = dateIn.getFullYear();
+	var mm = dateIn.getMonth() + 1; // getMonth() is zero-based
+	var dd = dateIn.getDate();
+	return ` ${dd}/${mm}/${yyyy}`;
+}
+
+const init = () => {
+	fetch("https://api.github.com/users/elliotlafave123")
+		.then((res) => res.json())
+		.then((data) => {
+			ShowGithubStats(data);
+		});
+};
+init();
+
+const ShowGithubStats = (data) => {
+	console.log(data);
+	let markup = `
+    <img src="./img/logo-dark.png" alt="" />
+    <div class="githubStats-container">
+        <div class="githubStats-top">
+            <h2>${data.name}</h2>
+            <p>Joined ${yyyymmdd(new Date(data.created_at))}</p>
+        </div>
+
+        <span>@${data.login}</span>
+        <p>${data.bio.slice(0, 142)}.</p>
+
+        <div class="githubStats-stats">
+            <div class="githubStats-stats-section">
+                <p>Repos</p>
+                <span>${data.public_repos}</span>
+            </div>
+            <div class="githubStats-stats-section">
+                <p>Followers</p>
+                <span>${data.followers}</span>
+            </div>
+            <div class="githubStats-stats-section">
+                <p>Commits</p>
+                <span>1498</span>
+            </div>
+        </div>
+
+        <div class="githubStats-info">
+            <div class="githubStats-info-section">
+                <i class="fa-solid fa-location-dot"></i>
+                <p>Manchester, UK</p>
+            </div>
+            <div class="githubStats-info-section">
+                <i class="fa-brands fa-twitter"></i>
+                <p>${data.login}</p>
+            </div>
+            <div class="githubStats-info-section">
+                <i class="fa-solid fa-link"></i>
+                <p>${data.blog}</p>
+            </div>
+            <div class="githubStats-info-section">
+                <i class="fa-solid fa-city"></i>
+                <p>@GMC</p>
+            </div>
+        </div>
+    </div>
+    `;
+
+	GithubStatsContainer.innerHTML = markup;
+};
