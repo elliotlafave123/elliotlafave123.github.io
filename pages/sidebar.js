17a1dcad68f1d19132797314ea1f869092d5c720
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-07-18 15:31:22 +0100

    .

--- pages/sidebar.js
+++ pages/sidebar.js
@@ -4,11 +4,17 @@ const clearAll = () => {
 	});
 };
 
+const hideSidebar = () => {
+	document.querySelector(".article__sidebar").classList.add("invisible");
+};
+const showSidebar = () => {
+	document.querySelector(".article__sidebar").classList.remove("invisible");
+};
+
 window.addEventListener("DOMContentLoaded", () => {
 	const observer = new IntersectionObserver((entries) => {
 		entries.forEach((entry) => {
 			const id = entry.target.getAttribute("id");
-			console.log(id);
 			if (entry.intersectionRatio > 0) {
 				document.querySelector(`ol li a[href="#${id}"]`).parentElement.classList.add("active");
 			} else {
@@ -22,3 +28,19 @@ window.addEventListener("DOMContentLoaded", () => {
 		observer.observe(section);
 	});
 });
+
+var myInterval = setInterval(function () {
+	let sectionHovered = false;
+	console.log(document.querySelectorAll(`ol li a`)[0].parentElement.classList.contains("active"));
+	document.querySelectorAll(`ol li a`).forEach((el) => {
+		if (el.parentElement.classList.contains("active")) {
+			sectionHovered = true;
+		}
+	});
+
+	if (sectionHovered) {
+		showSidebar();
+	} else {
+		hideSidebar();
+	}
+}, 50);
