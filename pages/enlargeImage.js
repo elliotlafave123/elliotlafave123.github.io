99e42075b8430081c20f6d0b2367d98199550166
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-09-28 20:12:40 +0100

    Refactored Image enlargement code

--- pages/enlargeImage.js
+++ pages/enlargeImage.js
@@ -1,12 +1,16 @@
 let enlargedImageOpen = false;
+let enlargedImageClass = "article__image-enlarged";
+let articleImageButtonClass = "article__image";
+let articleImageClass = "articleImg";
 
 document.addEventListener("click", function (e) {
-	if (e.target.classList.contains("article__image") || e.target.classList.contains("articleImg")) {
+	if (e.target.classList.contains(articleImageClass) || e.target.classList.contains(articleImageButtonClass)) {
+		let image = e.target.closest(`.${articleImageButtonClass}`);
 		if (!enlargedImageOpen) {
-			e.target.closest(".article__image").classList.add("article__image-enlarged");
+			image.classList.add(enlargedImageClass);
 			enlargedImageOpen = true;
 		} else {
-			e.target.closest(".article__image").classList.remove("article__image-enlarged");
+			image.classList.remove(enlargedImageClass);
 			enlargedImageOpen = false;
 		}
 	}
