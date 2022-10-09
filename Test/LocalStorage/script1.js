568823dc1d7792d794253d074b58b935fd788f67
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-10-09 12:55:15 +0100

    Add dark mode to ts bundle

--- Test/LocalStorage/script1.js
+++
@@ -1,8 +0,0 @@
-let played = localStorage.getItem("counterPlayed");
-
-if (localStorage.getItem("counterPlayed") === played) {
-	window.location.href = "counter.html";
-} else {
-	localStorage.setItem("incrementer", 0);
-	window.location.href = "counter.html";
-}
