54305c5cfaa900a7945bf21928802d161b25db1d
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-10-18 17:15:51 +0100

    Astro upgrade

---
+++ ElliotLaFave.com/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/TypeScript/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "./src/TypeScript")],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", "js"],
+  },
+  output: {
+    filename: "SiteFrontendTSBundle.js",
+    path: path.resolve(__dirname, "./public/scripts"),
+  },
+  mode: "production",
+};
