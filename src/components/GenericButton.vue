11b7f0ba34b5451295bc65b0834aeb010072b5f5
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2022-10-24 17:58:11 +0100

    Generic button component added

---
+++ src/components/GenericButton.vue
@@ -0,0 +1,65 @@
+<script>
+export default {
+  name: "GenericButton",
+  props: {
+    href: String,
+    corner: String,
+    size: String,
+    color: String,
+    button: Boolean,
+  },
+  setup() {},
+};
+</script>
+
+<template>
+  <button v-if="button" :href="href" :class="[corner, size, color]">
+    <slot></slot>
+  </button>
+  <a v-else :href="href" :class="[corner, size, color]">
+    <slot></slot>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+a,
+button {
+  color: var(--color-white);
+  text-transform: uppercase;
+  text-decoration: none;
+  font-weight: 400;
+  display: inline-block;
+  -webkit-transition: all 0.2s;
+  transition: all 0.2s;
+  border: none;
+  position: relative;
+  cursor: pointer;
+
+  &.purple {
+    background-color: var(--color-primary);
+  }
+  &.grey {
+    background-color: var(--color-card-git);
+  }
+
+  &.round {
+    border-radius: 8rem;
+  }
+
+  &.square {
+    border-radius: 1rem;
+  }
+  &.large {
+    font-size: 3rem;
+    padding: 0.5rem 5rem;
+  }
+  &.medium {
+    font-size: 2.4rem;
+    padding: 0.6rem 4.5rem;
+  }
+  &.small {
+    font-size: 2.2rem;
+    padding: 0.4rem 3rem;
+  }
+}
+</style>
