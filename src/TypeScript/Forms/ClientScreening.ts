c5f0ae373fe35e20f97d4688f4c582adad3d332b
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-03-12 22:01:47 +0000

    Built ts with correct api url

--- src/TypeScript/Forms/ClientScreening.ts
+++ src/TypeScript/Forms/ClientScreening.ts
@@ -33,8 +33,8 @@ const InitClientScreeningForm = () => {
       clientScreening.productOrService = clientScreeningForm.productOrService.value;
       clientScreening.websiteOrApplication = clientScreeningForm.websiteOrApplication.value;
 
-      // const API_URL = "https://freelance-api.elliotlafave.com/ClientScreening";
-      const API_URL = "https://localhost:7135/ClientScreening";
+      const API_URL = "https://freelance-api.elliotlafave.com/ClientScreening";
+      // const API_URL = "https://localhost:7135/ClientScreening";
 
       fetch(API_URL, {
         method: "POST",
