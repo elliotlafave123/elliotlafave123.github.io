f20f597db510dec9192430d3d5b692bf7aa3ba9b
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-08-23 18:49:03 +0100

    Created breadcrumb, fixed spacing

--- src/TypeScript/Breadcrumb/breadcrumb.ts
+++ src/TypeScript/Breadcrumb/breadcrumb.ts
@@ -3,6 +3,17 @@ const breadcrumbNav = document.getElementById("breadcrumbNav");
 export function createBreadcrumb() {
   const pathnames = window.location.pathname.split("/").filter((x) => x);
 
+  // If you're on the homepage, exit early and remove the parent div if it has class 'c-container'.
+  if (
+    pathnames.length === 0 ||
+    (pathnames.length === 1 && (pathnames[0] === "index.html" || pathnames[0] === "index"))
+  ) {
+    if (breadcrumbNav?.parentElement?.classList.contains("c-container")) {
+      breadcrumbNav.parentElement.remove();
+    }
+    return;
+  }
+
   // Always append the "Home" breadcrumb item
   const homeBreadcrumb = document.createElement("a");
   homeBreadcrumb.href = "/";
@@ -15,6 +26,12 @@ export function createBreadcrumb() {
   breadcrumbNav?.appendChild(dividerHome);
 
   let fullPath = "";
+
+  // If there are 5 segments in the path, remove the second to last
+  if (pathnames.length === 5) {
+    pathnames.splice(-2, 1);
+  }
+
   for (let i = 0; i < pathnames.length; i++) {
     const isLastItem = i === pathnames.length - 1;
     fullPath += "/" + pathnames[i];
