6fba0c0aa0ff38bb93fc2b7fae0be05cd2f53af6
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-08-24 17:38:00 +0100

    Breadcrumb collapse on mobile

--- src/TypeScript/Breadcrumb/breadcrumb.ts
+++ src/TypeScript/Breadcrumb/breadcrumb.ts
@@ -1,5 +1,7 @@
 const breadcrumbNav = document.getElementById("breadcrumbNav");
 
+let resizeTimeout: ReturnType<typeof setTimeout> | null = null;
+
 export function createBreadcrumb() {
   const pathnames = window.location.pathname.split("/").filter((x) => x);
 
@@ -14,6 +16,41 @@ export function createBreadcrumb() {
     return;
   }
 
+  // Append the items normally first
+  appendBreadcrumbItems(pathnames);
+
+  // Use a promise to delay checking of wrapping
+  Promise.resolve().then(() => {
+    // Check if wrapping occurred
+    const afterAppendHeight = breadcrumbNav?.offsetHeight || 0;
+
+    if (afterAppendHeight > 26) {
+      breadcrumbNav.innerHTML = ""; // Clear the breadcrumbNav
+      appendReducedBreadcrumbItems(pathnames);
+    }
+  });
+
+  window.addEventListener("resize", handleResize);
+}
+
+function handleResize() {
+  // If there's an existing timeout, clear it
+  if (resizeTimeout !== null) {
+    clearTimeout(resizeTimeout);
+  }
+
+  // Set a new timeout
+  resizeTimeout = setTimeout(() => {
+    const afterAppendHeight = breadcrumbNav?.offsetHeight || 0;
+
+    if (afterAppendHeight > 26) {
+      breadcrumbNav.innerHTML = ""; // Clear the breadcrumbNav
+      appendReducedBreadcrumbItems(window.location.pathname.split("/").filter((x) => x));
+    }
+  }, 300); // 300ms delay
+}
+
+function appendBreadcrumbItems(pathnames: string[]) {
   // Always append the "Home" breadcrumb item
   const homeBreadcrumb = document.createElement("a");
   homeBreadcrumb.href = "/";
@@ -57,4 +94,48 @@ export function createBreadcrumb() {
       breadcrumbNav?.appendChild(divider);
     }
   }
+}
+
+function appendReducedBreadcrumbItems(pathnames: string[]) {
+  if (pathnames.length <= 2) return appendBreadcrumbItems(pathnames);
+
+  // Append "Home"
+  const homeBreadcrumb = document.createElement("a");
+  homeBreadcrumb.href = "/";
+  homeBreadcrumb.textContent = "Home";
+  breadcrumbNav?.appendChild(homeBreadcrumb);
+
+  // Append divider before "..."
+  const dividerBeforeEllipsis = document.createElement("span");
+  dividerBeforeEllipsis.className = "divider";
+  dividerBeforeEllipsis.innerHTML = `<i class="fas fa-chevron-right"></i>`;
+  breadcrumbNav?.appendChild(dividerBeforeEllipsis);
+
+  // Append "..."
+  const ellipsis = document.createElement("span");
+  ellipsis.textContent = "...";
+  breadcrumbNav?.appendChild(ellipsis);
+
+  // Append divider after "..."
+  const dividerAfterEllipsis = document.createElement("span");
+  dividerAfterEllipsis.className = "divider";
+  dividerAfterEllipsis.innerHTML = `<i class="fas fa-chevron-right"></i>`;
+  breadcrumbNav?.appendChild(dividerAfterEllipsis);
+
+  // Append second-to-last item as a link
+  const secondLastItem = document.createElement("a");
+  secondLastItem.href = `/${pathnames.slice(0, -1).join("/")}`;
+  secondLastItem.textContent = decodeURIComponent(pathnames[pathnames.length - 2].replace(".html", ""));
+  breadcrumbNav?.appendChild(secondLastItem);
+
+  // Append divider
+  const divider = document.createElement("span");
+  divider.className = "divider";
+  divider.innerHTML = `<i class="fas fa-chevron-right"></i>`;
+  breadcrumbNav?.appendChild(divider);
+
+  // Append last item
+  const lastItem = document.createElement("span");
+  lastItem.textContent = decodeURIComponent(pathnames[pathnames.length - 1].replace(".html", ""));
+  breadcrumbNav?.appendChild(lastItem);
 }
