f6b1fe87dfa6c69f3acec8fbfc8fddbf64c8bd66
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-08-29 22:46:49 +0100

    Added promo card vue component

---
+++ src/components/Promo Card/PromoCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="link" class="c-promo-card">
+    <div class="c-promo-card__content">
+      <p class="c-promo-card__text">{{ text }}</p>
+      <div class="card-icon">
+        <i class="fa-solid fa-chevron-right"></i>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    link: {
+      type: String,
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
