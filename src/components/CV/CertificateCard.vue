e9db08104b467a3752b2fd8fa8d1eb37dcc84b64
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-08-29 22:04:47 +0100

    Added certificate card vue component

---
+++ src/components/CV/CertificateCard.vue
@@ -0,0 +1,31 @@
+<template>
+  <a :href="href" target="_blank">
+    <div class="c-certificate">
+      <img :src="imgSrc" :alt="imgAlt" />
+      <p class="c-certificate__title">{{ title }}</p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    href: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    imgAlt: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
