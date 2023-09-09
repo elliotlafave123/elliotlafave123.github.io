f45e4aa8814f9dc1b0111ca16c26d6560a47f963
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-09-09 18:05:07 +0100

    Fixed header auth links positioning

--- src/TypeScript/Authentication/Views/Login/UpdateHeaderAuth.ts
+++ src/TypeScript/Authentication/Views/Login/UpdateHeaderAuth.ts
@@ -4,9 +4,9 @@ const headerAuthLoggedIn = document.getElementById("headerAuthLoggedIn");
 export const updateHeaderAuth = (isLoggedIn: boolean) => {
   if (isLoggedIn) {
     headerAuthLoggedOut.style.display = "none";
-    headerAuthLoggedIn.style.display = "block";
+    headerAuthLoggedIn.style.display = "flex";
   } else {
-    headerAuthLoggedOut.style.display = "block";
+    headerAuthLoggedOut.style.display = "flex";
     headerAuthLoggedIn.style.display = "none";
   }
 };
