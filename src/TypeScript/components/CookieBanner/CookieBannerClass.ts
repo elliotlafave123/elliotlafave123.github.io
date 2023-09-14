b5840332649b1dc6199ca454dd52bb4784aec781
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-09-14 14:57:59 +0100

    Updated cookie banner design and logic

---
+++ src/TypeScript/components/CookieBanner/CookieBannerClass.ts
@@ -0,0 +1,66 @@
+export class CookieBanner {
+  public cookieBanner: HTMLElement;
+  private acceptCookiesButton: HTMLElement;
+  private denyCookiesButton: HTMLElement;
+  private minimizeBannerButton: HTMLElement;
+  private expandBannerButton: HTMLElement;
+  private policyLinks: NodeListOf<HTMLAnchorElement>;
+
+  constructor() {
+    this.cookieBanner = document.querySelector(".c-cookie-banner");
+    this.acceptCookiesButton = document.getElementById("js-accept-cookies");
+    this.denyCookiesButton = document.getElementById("js-deny-cookies");
+    this.minimizeBannerButton = document.getElementById("js-minimize-banner");
+    this.expandBannerButton = document.getElementById("js-expand-banner");
+    this.policyLinks = document.querySelectorAll(".c-cookie-banner a");
+    this.cookieBanner.classList.remove("c-cookie-banner--hidden");
+
+    this.initEventListeners();
+    this.initStates();
+  }
+
+  public minimizeBanner = (): void => {
+    this.cookieBanner.classList.add("c-cookie-banner--minimized");
+    localStorage.setItem("cookiesMinimized", "true");
+  };
+
+  public expandBanner = (): void => {
+    this.cookieBanner.classList.remove("c-cookie-banner--minimized");
+    localStorage.setItem("cookiesMinimized", "false");
+  };
+
+  public acceptCookies = async (): Promise<void> => {
+    console.log("Accepting cookies");
+    localStorage.setItem("cookiesAccepted", "true");
+    this.cookieBanner.classList.add("c-cookie-banner--hidden");
+    window.location.reload();
+  };
+
+  public denyCookies = async (): Promise<void> => {
+    localStorage.setItem("cookiesAccepted", "false");
+    localStorage.setItem("cookiesDenied", "true");
+    this.cookieBanner.classList.add("c-cookie-banner--hidden");
+    window.location.reload();
+  };
+
+  private initEventListeners(): void {
+    this.acceptCookiesButton.addEventListener("click", this.acceptCookies);
+    this.denyCookiesButton.addEventListener("click", this.denyCookies);
+    this.minimizeBannerButton.addEventListener("click", this.minimizeBanner);
+    this.expandBannerButton.addEventListener("click", this.expandBanner);
+
+    this.policyLinks.forEach((link) => {
+      link.addEventListener("click", this.minimizeBanner);
+    });
+  }
+
+  private initStates(): void {
+    if (localStorage.getItem("cookiesMinimized") === "true") {
+      this.cookieBanner.classList.add("c-cookie-banner--minimized");
+    }
+
+    if (localStorage.getItem("cookiesAccepted") === "true") {
+      this.cookieBanner.classList.add("c-cookie-banner--hidden");
+    }
+  }
+}
