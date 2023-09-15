7c57cd01c7c5b03c6298bc39c650dc4116288dab
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-09-15 15:00:00 +0100

    Added account details page

--- src/TypeScript/Authentication/Views/Account/AccountPage.ts
+++ src/TypeScript/Authentication/Views/Account/AccountPage.ts
@@ -1,21 +1,13 @@
-import { Modal } from "../../../../../ComponentAssets/simpleComponents/modal/modal";
-import { updateUser } from "../../Controllers/Account/updateUser";
+import { GenerateDateString } from "../../../helpers/generateDateString";
 import { CheckLogin } from "../../Controllers/Login/CheckLogin";
-import { DeleteUser } from "../../Controllers/Me/DeleteUser";
 import { getCurrentUser } from "../../Controllers/Me/GetCurrentUser";
-import { initProfileMenu } from "./Menu/AccountMenu";
-import { addDataToInputs } from "./addDataToInputs";
-import { showAccountStatusMessage } from "./showAccountStatusMessage";
+import { UserProfileInput } from "../../Controllers/Profile/userProfile";
 
 async function InitAccountPage() {
   const hash = window.location.hash;
 
-  if (window.innerWidth < 900 && !hash) {
-    window.location.href = "/account-home.html";
-  } else {
-    if (!hash) {
-      window.location.hash = "#profile";
-    }
+  if (!hash) {
+    window.location.hash = "#profile";
   }
 
   const signedIn = await CheckLogin();
@@ -24,72 +16,48 @@ async function InitAccountPage() {
     window.location.href = "/pages/login/login.html";
   }
 
-  // const user = await getCurrentUser();
-  // if (user) {
-  //   addDataToInputs(user.firstName, user.lastName, user.displayName, user.email);
-  // }
+  const user = await getCurrentUser();
+  const userProfile = {
+    fullName: `${user.firstName} ${user.lastName}`,
+    displayName: user.displayName,
+    letter: user.firstName[0],
+    joinedOn: GenerateDateString(user.createdAt),
+  };
 
-  // const saveChangesButton = document.getElementById("SaveChanges");
-  // if (saveChangesButton) {
-  //   saveChangesButton.addEventListener("click", async () => {
-  //     const firstName = document.getElementById("firstname") as HTMLInputElement;
-  //     const lastName = document.getElementById("lastname") as HTMLInputElement;
-  //     const displayName = document.getElementById("displayname") as HTMLInputElement;
-  //     const email = document.getElementById("email") as HTMLInputElement;
-
-  //     const user = {
-  //       firstName: firstName.value,
-  //       lastName: lastName.value,
-  //       displayName: displayName.value,
-  //       email: email.value,
-  //     };
-
-  //     if (user) {
-  //       const updated = await updateUser(user);
-  //       console.log(updated);
-
-  //       if (updated) {
-  //         showAccountStatusMessage(true);
-  //         addDataToInputs(updated.firstName, updated.lastName, updated.displayName, updated.email);
-  //         window.scrollTo(0, 0);
-  //       } else {
-  //         showAccountStatusMessage(false);
-  //       }
-  //     }
-  //   });
-  // }
-
-  // const deleteAccountButton = document.getElementById("DeleteAccountButton");
-  // if (deleteAccountButton) {
-  //   deleteAccountButton.addEventListener("click", async () => {
-  //     const modal = new Modal("Delete comment", "Are you sure you want to delete this comment?", "Delete", "Cancel");
-
-  //     try {
-  //       const result = await modal.open({
-  //         title: "Delete account",
-  //         paragraph: "Are you sure you want to delete your account?",
-  //         confirmText: "Delete",
-  //         cancelText: "Cancel",
-  //         size: "medium",
-  //         corner: "round",
-  //         colour: "primary",
-  //         withBorder: true,
-  //         ariaLabel: "Delete account modal",
-  //       });
-
-  //       if (result === "confirm") {
-  //         const deleted = await DeleteUser();
-  //         if (deleted) {
-  //           localStorage.removeItem("token");
-  //           window.location.href = "/index.html";
-  //         } else {
-  //           alert("Error deleting account, please try again later");
-  //         }
-  //       }
-  //     } catch (error) {
-  //       console.error("Modal action was cancelled", error);
-  //     }
-  //   });
-  // }
+  fillUserProfile(userProfile);
+  fillPersonalInformation(user);
 }
 InitAccountPage();
+
+function fillPersonalInformation(userProfile) {
+  console.log(userProfile);
+  document.querySelector(".js-userDetails-firstName").textContent = userProfile.firstName;
+  document.querySelector(".js-userDetails-lastName").textContent = userProfile.lastName;
+  document.querySelector(".js-userDetails-displayName").textContent = userProfile.displayName.toLowerCase();
+  document.querySelector(".js-userDetails-email").textContent = userProfile.email;
+}
+
+function fillUserProfile(userProfile: UserProfileInput) {
+  const fullName = document.querySelectorAll(".js-userProfile-fullName");
+  const displayname = document.querySelectorAll(".js-userProfile-displayname");
+  const letter = document.querySelectorAll(".js-userProfile-letter");
+  const joinedOn = document.querySelectorAll(".js-userProfile-joinedOn");
+
+  fullName.forEach((element) => {
+    element.textContent = userProfile.fullName;
+  });
+
+  displayname.forEach((element) => {
+    element.textContent = "@" + userProfile.displayName.toLowerCase();
+  });
+
+  letter.forEach((element) => {
+    element.textContent = userProfile.letter;
+  });
+
+  if (userProfile.joinedOn) {
+    joinedOn.forEach((element) => {
+      element.textContent = "Joined:" + userProfile.joinedOn;
+    });
+  }
+}
