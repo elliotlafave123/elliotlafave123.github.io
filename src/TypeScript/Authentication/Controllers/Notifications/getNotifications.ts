bd625fdd5de3f4ee53ba8249f13ea541557f3d60
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-09-17 20:36:47 +0100

    Added notifications

---
+++ src/TypeScript/Authentication/Controllers/Notifications/getNotifications.ts
@@ -0,0 +1,29 @@
+import { Constants } from "../../../Constants/Constants";
+import { NotificationModel } from "../../Models/NotificationsModel";
+
+export async function getNotifications() {
+  try {
+    const token = localStorage.getItem("token");
+    const response = await fetch(`${Constants.API_BASE_URL}/api/users/notifications`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response);
+
+    if (response.status === 404) {
+      return null;
+    }
+
+    try {
+      const data = await response.json();
+      return data as Array<NotificationModel>;
+    } catch (error) {
+      throw new Error("Error parsing response");
+    }
+  } catch (error) {
+    return null;
+  }
+}
