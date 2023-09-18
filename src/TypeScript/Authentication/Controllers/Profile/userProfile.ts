02b4635b0abdabd4338332be5fed153803912440
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2023-09-18 14:20:51 +0100

    Added ability to mark comments as read.

--- src/TypeScript/Authentication/Controllers/Profile/userProfile.ts
+++ src/TypeScript/Authentication/Controllers/Profile/userProfile.ts
@@ -37,7 +37,7 @@ export function insertUserProfileData(userProfile: UserProfileInput) {
   if (fullName && displayname && letter) {
     fullName.textContent = userProfile.fullName;
     displayname.textContent = "@" + userProfile.displayName.toLowerCase();
-    letter.textContent = userProfile.letter;
+    letter.textContent = userProfile.letter.toUpperCase();
   }
 
   if (window.innerWidth < 900) {
