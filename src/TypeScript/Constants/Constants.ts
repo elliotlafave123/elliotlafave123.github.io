fa3d6e620f9bec13a25be5a36e3936be111c5f56
Elliot La Fave <44415567+elliotlafave123@users.noreply.github.com>
2024-03-17 19:56:29 +0000

    Chamged API Server IP

--- src/TypeScript/Constants/Constants.ts
+++ src/TypeScript/Constants/Constants.ts
@@ -3,7 +3,7 @@ const env = document.getElementById("jsEnvironment")?.textContent?.trim() || "de
 export class Constants {
   static API_BASE_URL =
     env === "production"
-      ? "https://elliotapiserver.com"
+      ? "https://api.elliotlafave.com"
       : env === "test"
       ? "https://test.elliotapiserver.com"
       : "http://localhost:5001";
